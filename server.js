651d0e3021f76c56053ecd629eab4c52a43a31f2
charlie <152432127+ch4rlesexe@users.noreply.github.com>
2024-12-06 13:35:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const cors = require('cors'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+
 app.get('/check/:username', async (req, res) => {
-    const username = req.params.username.toLowerCase(); 
+    const username = req.params.username.toLowerCase();
     try {
         const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
         if (response.ok) {
@@ -16,7 +19,7 @@ app.get('/check/:username', async (req, res) => {
             res.status(response.status).json({ username, status: `Error: ${response.status}` });
         }
     } catch (error) {
-        res.status(500).json({ username, status: 'Network Error' });
+        res.status(500).json({ username, status: 'Network Error -_-' });
     }
 });
 
